562f24c80cc88983f7af8c9970e3ce88a6054ae0
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-08-28 20:10:15 +0530

    home-screen done

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import StackNavigator from "./StackNavigator";
+import { ModalPortal } from "react-native-modals";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <Provider store={store}> */}
+      <StackNavigator />
+      <ModalPortal />
+      {/* </Provider> */}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     textAlign: "center",
+//   },
+// });
--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,11 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import SavedScreen from "./screens/SavedScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import BookingScreen from "./screens/BookingScreen";
 
 const StackNavigator = () => {
   const Tab = createBottomTabNavigator();
@@ -14,15 +19,80 @@ const StackNavigator = () => {
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{ tabBarLabel: "Home", headerShown: false }}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="#003580" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Saved"
+          component={SavedScreen}
+          options={{
+            tabBarLabel: "Saved",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <AntDesign name="heart" size={24} color="#003580" />
+              ) : (
+                <AntDesign name="hearto" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Bookings"
+          component={BookingScreen}
+          options={{
+            tabBarLabel: "Bookings",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="notifications" size={24} color="#003580" />
+              ) : (
+                <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="black"
+                />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person" size={24} color="#003580" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="black" />
+              ),
+          }}
         />
       </Tab.Navigator>
     );
   }
+
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
